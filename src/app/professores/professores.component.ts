99e5b7ab3b079e9a664f1fb26e336867e7b9b765
frederico101 <fredyalves.fredyalves@hotmail.com>
2020-08-19 17:54:40 -0300

    Tabela de professores mock

--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -9,11 +9,21 @@ export class ProfessoresComponent implements OnInit {
 
   constructor() { }
   public title = "Professores";
+  public profSelecionado: string;
   public professores = [
     { id: 1, name: 'Frederico', secondeName: 'brito', Age: '35', disciplina:'programanção' },
     { id: 2, name: 'Aline', secondeName: 'brito', Age: '35'    , disciplina:'literatura' },
     { id: 3, name: 'Jesus', secondeName: 'Cristo', Age: '70*7' , disciplina:'biologia' }
   ];
+
+  profSelect(professor: any)
+  {
+    this.profSelecionado = professor.name; 
+  }
+  voltar()
+  {
+    this.profSelecionado = '';
+  }
 
   ngOnInit() {
   }
